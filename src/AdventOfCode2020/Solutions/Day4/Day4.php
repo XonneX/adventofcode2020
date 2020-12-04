8e40713ec215c725b2c9c8b0a592a0f40e768f60
XonneX <baumannjanik@gmail.com>
2020-12-04 08:22:04 +0100

    Day 4

--- src/AdventOfCode2020/Solutions/Day4/Day4.php
+++ src/AdventOfCode2020/Solutions/Day4/Day4.php
@@ -4,11 +4,21 @@ declare(strict_types=1);
 
 namespace XonneX\AdventOfCode2020\Solutions\Day4;
 
-use RuntimeException;
 use XonneX\AdventOfCode2020\Solutions\AbstractSolution;
+
+use function array_key_exists;
+use function explode;
+use function in_array;
+use function preg_match;
+use function str_ends_with;
+use function substr;
 
 class Day4 extends AbstractSolution
 {
+    private const EYE_COLORS = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
+
+    private array $passports;
+
     public function __construct()
     {
         parent::__construct(2020, 4);
@@ -16,11 +26,163 @@ class Day4 extends AbstractSolution
 
     protected function partOne(string $input): string
     {
-        throw new RuntimeException('Not implemented yet');
+        $this->parsePassports($input);
+
+        $requiredFields = [
+            'byr',
+            'iyr',
+            'eyr',
+            'hgt',
+            'hcl',
+            'ecl',
+            'pid',
+        ];
+
+        $validPassportsCounter = 0;
+        foreach ($this->passports as $passport) {
+            $valid = true;
+
+            foreach ($requiredFields as $key) {
+                if (!array_key_exists($key, $passport)) {
+                    $valid = false;
+
+                    break;
+                }
+            }
+
+            if ($valid) {
+                $validPassportsCounter++;
+            }
+        }
+
+        return (string) $validPassportsCounter;
     }
 
     protected function partTwo(string $input): string
     {
-        throw new RuntimeException('Not implemented yet');
+        $this->parsePassports($input);
+
+        $validPassportsCounter = 0;
+        foreach ($this->passports as $passport) {
+            if ($this->validatePassport($passport)) {
+                $validPassportsCounter++;
+            }
+        }
+
+        return (string) $validPassportsCounter;
+    }
+
+    private function parsePassports(string $input): void
+    {
+        $lines           = explode("\n", $input);
+        $this->passports = [];
+        $currentPassport = null;
+
+        foreach ($lines as $line) {
+            if ($line === '') {
+                $this->passports[] = $currentPassport;
+                $currentPassport   = null;
+                continue;
+            }
+
+            if ($currentPassport === null) {
+                $currentPassport = [];
+            }
+
+            $map = explode(' ', $line);
+
+            foreach ($map as $field) {
+                [$key, $value] = explode(':', $field);
+
+                $currentPassport[$key] = $value;
+            }
+        }
+
+        if ($currentPassport !== null) {
+            $this->passports[] = $currentPassport;
+        }
+    }
+
+    public function validatePassport(mixed $passport): bool
+    {
+        if (!array_key_exists('byr', $passport)) {
+            return false;
+        }
+
+        $byr = (int) $passport['byr'];
+
+        if ($byr < 1920 || $byr > 2002) {
+            return false;
+        }
+
+        if (!array_key_exists('iyr', $passport)) {
+            return false;
+        }
+
+        $iyr = (int) $passport['iyr'];
+
+        if ($iyr < 2010 || $iyr > 2020) {
+            return false;
+        }
+
+        if (!array_key_exists('eyr', $passport)) {
+            return false;
+        }
+
+        $eyr = (int) $passport['eyr'];
+
+        if ($eyr < 2020 || $eyr > 2030) {
+            return false;
+        }
+
+        if (!array_key_exists('hgt', $passport)) {
+            return false;
+        }
+
+        $hgt = $passport['hgt'];
+
+        if (str_ends_with($hgt, 'cm')) {
+            $hgtAmount = (int) substr($hgt, 0, 3);
+
+            if ($hgtAmount < 150 || $hgtAmount > 193) {
+                return false;
+            }
+        } elseif (str_ends_with($hgt, 'in')) {
+            $hgtAmount = (int) substr($hgt, 0, 2);
+
+            if ($hgtAmount < 59 || $hgtAmount > 76) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        if (!array_key_exists('hcl', $passport)) {
+            return false;
+        }
+
+        $hcl = $passport['hcl'];
+
+        if (preg_match('/^#[0-9a-f]{6}$/', $hcl) !== 1) {
+            return false;
+        }
+
+        if (!array_key_exists('ecl', $passport)) {
+            return false;
+        }
+
+        $ecl = $passport['ecl'];
+
+        if (!in_array($ecl, self::EYE_COLORS, true)) {
+            return false;
+        }
+
+        if (!array_key_exists('pid', $passport)) {
+            return false;
+        }
+
+        $pid = $passport['pid'];
+
+        return !(preg_match('/^[\d]{9}$/', $pid) !== 1);
     }
 }
--- tests/AdventOfCode2020/Solutions/Day4/Day4Test.php
+++ tests/AdventOfCode2020/Solutions/Day4/Day4Test.php
@@ -12,39 +12,152 @@ class Day4Test extends TestCase
     {
         $day4 = new Day4();
 
-        /** @noinspection SpellCheckingInspection */
-        $day4->setDebugInput(<<<TXT
-NO_EXAMPLE_INITIALIZED
+        $day4->setDebugInput(
+            <<<TXT
+ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in
 TXT
         );
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day4->solvePartOne());
+        self::assertSame('2', $day4->solvePartOne());
+    }
+
+    public function testSolvePartOneWrongAnswerOne(): void
+    {
+        $day2 = new Day4();
+
+        self::assertNotSame('169', $day2->solvePartOne());
+        self::assertTrue(((int) $day2->solvePartOne()) > 169);
     }
 
     public function testSolvePartOne(): void
     {
         $day4 = new Day4();
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day4->solvePartOne());
+        self::assertSame('170', $day4->solvePartOne());
     }
 
-    public function testSolvePartTwoExample(): void
+    public function testSolvePartTwoExampleOne(): void
     {
         $day4 = new Day4();
 
-        /** @noinspection SpellCheckingInspection */
-        $day4->setDebugInput(<<<TXT
-NO_EXAMPLE_INITIALIZED
+        $testPassport = [
+            'byr' => '2002',
+            'iyr' => '2015',
+            'eyr' => '2025',
+            'hgt' => '60in',
+            'hcl' => '#123abc',
+            'ecl' => 'brn',
+            'pid' => '000000001',
+        ];
+
+        self::assertTrue($day4->validatePassport($testPassport));
+
+        $passport = $testPassport;
+        $passport['byr'] = '2003';
+        self::assertFalse($day4->validatePassport($passport));
+
+        $passport = $testPassport;
+        $passport['hgt'] = '190cm';
+        self::assertTrue($day4->validatePassport($passport));
+
+        $passport = $testPassport;
+        $passport['hgt'] = '190in';
+        self::assertFalse($day4->validatePassport($passport));
+
+        $passport = $testPassport;
+        $passport['hgt'] = '190';
+        self::assertFalse($day4->validatePassport($passport));
+
+        $passport = $testPassport;
+        $passport['hcl'] = '#123abz';
+        self::assertFalse($day4->validatePassport($passport));
+
+        $passport = $testPassport;
+        $passport['hcl'] = '123abc';
+        self::assertFalse($day4->validatePassport($passport));
+
+        $passport = $testPassport;
+        $passport['ecl'] = 'wat';
+        self::assertFalse($day4->validatePassport($passport));
+
+        $passport = $testPassport;
+        $passport['pid'] = '0123456789';
+        self::assertFalse($day4->validatePassport($passport));
+    }
+
+    public function testSolvePartTwoExampleTwo(): void
+    {
+        $day4 = new Day4();
+
+        $day4->setDebugInput(
+            <<<TXT
+eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
 TXT
         );
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day4->solvePartTwo());
+        self::assertSame('0', $day4->solvePartTwo());
+    }
+
+    public function testSolvePartTwoExampleThree(): void
+    {
+        $day4 = new Day4();
+
+        $day4->setDebugInput(
+            <<<TXT
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+TXT
+        );
+
+        self::assertSame('4', $day4->solvePartTwo());
+    }
+
+    public function testSolvePartTwoWrongAnswerOne(): void
+    {
+        $day4 = new Day4();
+
+        self::assertNotSame('309', $day4->solvePartTwo());
+        self::assertTrue(((int) $day4->solvePartTwo()) < 309);
     }
 
     public function testSolvePartTwo(): void
     {
         $day4 = new Day4();
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day4->solvePartTwo());
+        self::assertSame('103', $day4->solvePartTwo());
     }
 }
