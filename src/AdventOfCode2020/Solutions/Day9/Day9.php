cbd05843dbf2dfa42e1aaa2f7ee7b5423248962c
XonneX <XonneX@users.noreply.github.com>
2020-12-09 07:33:48 +0100

    Add day 9

--- src/AdventOfCode2020/Solutions/Day9/Day9.php
+++ src/AdventOfCode2020/Solutions/Day9/Day9.php
@@ -16,11 +16,90 @@ class Day9 extends AbstractSolution
 
     protected function partOne(string $input): string
     {
-        throw new RuntimeException('Not implemented yet');
+        $lines = explode("\n", $input);
+
+        return $this->findNotMatchingNumber(25, $lines);
+    }
+
+    /**
+     * @param non-empty-list<string> $lines
+     */
+    public function findNotMatchingNumber(int $preambleSize, array $lines): string
+    {
+        $key = $preambleSize;
+
+        while (true) {
+            $checkKeys = range($key - $preambleSize, $key - 1);
+
+
+            $tmpNumbers = [];
+            foreach ($checkKeys as $checkKey) {
+                $tmpNumbers[] = (int) $lines[$checkKey];
+            }
+
+            $tmpSums = [];
+            foreach ($tmpNumbers as $tmpNumber) {
+                foreach ($tmpNumbers as $tmpNumber2) {
+                    if ($tmpNumber === $tmpNumber2) {
+                        continue;
+                    }
+
+                    $tmpSums[] = $tmpNumber + $tmpNumber2;
+                }
+            }
+
+            $sum = (int) $lines[$key];
+
+            if (!in_array($sum, $tmpSums, true)) {
+                return (string) $sum;
+            }
+
+            $key++;
+        }
     }
 
     protected function partTwo(string $input): string
     {
-        throw new RuntimeException('Not implemented yet');
+        $lines = explode("\n", $input);
+
+        return $this->findSumOfNotMatchingNumber(25, $lines);
+    }
+
+    /**
+     * @param non-empty-list<string> $lines
+     */
+    public function findSumOfNotMatchingNumber(int $preambleSize, array $lines): string
+    {
+        $notMatchingNumber = (int) $this->findNotMatchingNumber($preambleSize, $lines);
+
+        $numbers = [];
+        $sum     = 0;
+        foreach ($lines as $key => $line) {
+            $i = $key;
+            while (true) {
+                $number = (int) $lines[$i];
+
+                if (($sum + $number) === $notMatchingNumber) {
+                    sort($numbers);
+
+                    $number1 = array_shift($numbers);
+                    $number2 = array_pop($numbers);
+
+                    return (string) ($number1 + $number2);
+                }
+
+                if ($sum > $notMatchingNumber) {
+                    $numbers = [];
+                    $sum     = 0;
+                    break;
+                }
+
+                $i++;
+                $sum       += $number;
+                $numbers[] = $number;
+            }
+        }
+
+        throw new RuntimeException('Oh no');
     }
 }
--- tests/AdventOfCode2020/Solutions/Day9/Day9Test.php
+++ tests/AdventOfCode2020/Solutions/Day9/Day9Test.php
@@ -12,39 +12,79 @@ class Day9Test extends TestCase
     {
         $day9 = new Day9();
 
-        /** @noinspection SpellCheckingInspection */
-        $day9->setDebugInput(<<<TXT
-NO_EXAMPLE_INITIALIZED
+        $lines = explode(
+            "\n",
+            <<<TXT
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
 TXT
         );
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day9->solvePartOne());
+        self::assertSame('127', $day9->findNotMatchingNumber(5, $lines));
     }
 
     public function testSolvePartOne(): void
     {
         $day9 = new Day9();
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day9->solvePartOne());
+        self::assertSame('29221323', $day9->solvePartOne());
     }
 
     public function testSolvePartTwoExample(): void
     {
         $day9 = new Day9();
 
-        /** @noinspection SpellCheckingInspection */
-        $day9->setDebugInput(<<<TXT
-NO_EXAMPLE_INITIALIZED
+        $lines = explode(
+            "\n",
+            <<<TXT
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
 TXT
         );
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day9->solvePartTwo());
+        self::assertSame('62', $day9->findSumOfNotMatchingNumber(5, $lines));
     }
 
     public function testSolvePartTwo(): void
     {
         $day9 = new Day9();
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day9->solvePartTwo());
+        self::assertSame('4389369', $day9->solvePartTwo());
     }
 }
